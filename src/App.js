cafc6986c98c0bdc22efc053f2356d694ec88068
Madelin Rangel <mrangel@utp.edu.co>
2020-08-08 16:53:23 -0500

    Created Products component and styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import data from './data.json';
+import Products from './components/Products';
 
 function App() {
+  const [products] = useState(data.products);
+
   return (
     <div className="grid-container">
       <header>
         <a href="/">Ecommerce Shopping Cart</a>
       </header>
-      <main>Product List</main>
+      <main>
+        <div className="content">
+          <div className="main">
+            <Products products={products} />
+          </div>
+          <div className="sidebar">Cart Items</div>
+        </div>
+      </main>
       <footer>All right is reserved</footer>
     </div>
   );
